7773527ad93ec5bf71419d361efa0db866826cca
bhoomikabn <bhoomikabn2002@gmail.com>
2026-02-07 13:19:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Footer from "./Components/Footer";
+import Main from "./Components/Main";
+import Navbar from "./Components/Navbar";
+import ProductCard from "./Components/ProductCards";
+import Products from "./Components/Products";
+
+function App() {
+  return (
+    <>
+      <Navbar headerName="React Mobile Store" />
+      <Main
+        h1="Welcome to the mobile store"
+        p="This mobile store website is built on reactJS"
+        buttonName="Get Started"
+      />
+      <Products />
+      
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Products.jsx
@@ -0,0 +1,22 @@
+import { mobiles } from "../Data/mbile_data";
+import ProductCard from "./ProductCards";
+
+function Products() {
+  return (
+    <div className="container my-4">
+      <h2 className="text-center md-4">Available mobile phones</h2>
+      <div className="row g-4">
+        {mobiles.map((mobile) => (
+          <ProductCard
+            key={mobile.id}
+            name={mobile.name}
+            price={mobile.price}
+            inStock={mobile.inStock}
+            image={mobile.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Products;
